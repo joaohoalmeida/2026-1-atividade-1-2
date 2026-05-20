7341b2353294dbda37ffac51fa127b6f6072fb41
joao <joaoheli99@gmail.com>
2026-05-19 23:24:09 -0300

    feat:  criacão do dockerfile e do docker-compose

--- docker-compose-echo-java/client/Client.java
+++ docker-compose-echo-java/client/Client.java
@@ -1,23 +1,21 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
 
     Client() throws IOException{
         
-        Socket socket = new Socket("localhost", 6789);
+        Socket socket = new Socket("echo-server", 6789);
         System.out.println("Conectado ao servidor!");
 
         BufferedReader in_socket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out_socket = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-        Scanner sc = new Scanner(System.in);
 
         String message = in_socket.readLine();
         System.out.println("O servidor enviou: " + message);
 
-        message = sc.nextLine();
-        out_socket.println(message);
+        String response = "Olá do cliente Java!";
+        out_socket.println(response);
 
         socket.close();
         System.out.println("Comunicacão fechada");
